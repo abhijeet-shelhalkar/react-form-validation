e7e6b715140d378746663ce8979cb703487b795c
abhijeet.shelhalkar <abhijeet.shelhalkar@hurix.com>
2022-03-04 22:34:11 +0530

    @Added(input): added changes in input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+
+  const [userName, setUserName] = useState('');
+
+  const onUserNameChangeHandler = (event) => {
+    setUserName(event.target.value);
+  }
+
+  const onFormSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log("user name:", userName);
+  }
+
   return (
-    <form>
+    <form onSubmit={onFormSubmissionHandler}>
       <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <label htmlFor='name'>Your Name:</label>
+        <input type='text' id='name' onChange={onUserNameChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
